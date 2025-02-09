d398e932f6a3a75170972e4ce26e26d9f01abb86
Jorki3 <eduardomttzt01@outlook.com>
2025-02-08 21:01:47 -0600

    Project Enum

---
+++ app/Enums/ProjectStatus.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Enums;
+
+enum ProjectStatus: int
+{
+    case INPROGRESS = 1;
+    case COMPLETED = 2;
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::INPROGRESS => 'In progress',
+            self::COMPLETED => 'Completed',
+        };
+    }
+
+    public function styles(): string
+    {
+        return match ($this) {
+            self::INPROGRESS => 'text-yellow-500',
+            self::COMPLETED => 'text-green-500',
+        };
+    }
+}
--- resources/views/livewire/project.blade.php
+++ resources/views/livewire/project.blade.php
@@ -1,11 +1,11 @@
 <div>
-    <div class="mb-4 space-y-2">
+    <div class="mb-4">
         <h1 class="text-2xl font-black text-zinc-100">{{ $project->name }}</h1>
+        <p class="mb-2 text-xs {{ $project->status->styles() }}">
+            {{ $project->status->label() }}
+        </p>
 
         <p class="text-zinc-300">{{ $project->description }}</p>
-        <p>
-            {{ $project->status }}
-        </p>
     </div>
 
 
