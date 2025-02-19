659e956027bbae9c62b60650e3a4a5e71ddeaa20
Jorki3 <eduardomttzt01@outlook.com>
2025-02-19 01:54:01 -0600

    Logout button

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -20,5 +20,24 @@
             <x-menu-item href="dashboard" title="Dashboard" />
             <x-menu-item href="projects" title="Projects" />
         </ul>
+
+        <!-- Button to logout, pushed to the bottom -->
+        <div class="mt-auto">
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <button type="submit"
+                    class="inline-flex items-center w-full p-2 mt-4 text-sm rounded-lg text-zinc-500 hover:bg-zinc-100 focus:outline-none focus:ring-2 focus:ring-zinc-200 dark:text-zinc-400 dark:hover:bg-zinc-700 dark:focus:ring-zinc-600">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" class="mr-2 icon icon-tabler icons-tabler-outline icon-tabler-logout">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+                        <path d="M9 12h12l-3 -3" />
+                        <path d="M18 15l3 -3" />
+                    </svg>
+                    Logout
+                </button>
+            </form>
+        </div>
     </div>
 </aside>
